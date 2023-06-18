73525f9f39327cf27972a412db9ed0c1f79444fc
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-06-18 17:02:31 +0530

    Update 3. Merge two sorted arrays.cpp

--- Day2: Arrays P-II/3. Merge two sorted arrays.cpp
+++ Day2: Arrays P-II/3. Merge two sorted arrays.cpp
@@ -4,24 +4,24 @@ vector<int> ninjaAndSortedArrays(vector<int>& arr1, vector<int>& arr2, int m, in
 	
 	// adding elements of arr2 to arr1
 	for(int i=0; i<n; i++){
-        arr1[m+i]=arr2[i];
-    }
-
+		arr1[m+i]=arr2[i];
+	}
+	
 	int len = n+m;
-    int gap= (len/2) + (len%2);
-
-    while(gap>=1){
-        int left=0, right=left+gap;
-        while(right<n+m){
-
-            if(arr1[left] > arr1[right]){
-                swap(arr1[left], arr1[right]);
-            }
-            left++; right++;
-        }
+	int gap= (len/2) + (len%2);
+	
+	while(gap>=1){
+		int left=0, right=left+gap;
+		while(right<len){
 		
-        if(gap==1) break;
-        gap=(gap+1)/2;
-    }
-    return arr1;    
-}
+		    if(arr1[left] > arr1[right]){
+			swap(arr1[left], arr1[right]);
+		    }
+		    left++; right++;
+		}
+			
+		if(gap==1) break;
+		gap=(gap+1)/2;
+	}
+	return arr1;    
+	}
