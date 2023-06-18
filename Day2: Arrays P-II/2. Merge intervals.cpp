3ef7339f73d5a852249902fd274a06dbb0e696c5
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-06-18 13:35:56 +0530

    Update 2. Merge intervals.cpp

--- Day2: Arrays P-II/2. Merge intervals.cpp
+++ Day2: Arrays P-II/2. Merge intervals.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h> 
+/*
 
+    intervals[i][0] = start point of i'th interval
+    intervals[i][1] = finish point of i'th interval
+
+*/
+
+vector<vector<int>> mergeIntervals(vector<vector<int>> &intervals)
+{
+    int n = intervals.size();
+    sort(intervals.begin(), intervals.end());
+    vector<vector<int>> ans;
+
+    for(int i=0; i<n; i++){
+        // creating a new interval
+        if(ans.empty() || intervals[i][0] > ans.back()[1])
+            ans.push_back(intervals[i]);
+        else
+            ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+    }
+    return ans;
+}
