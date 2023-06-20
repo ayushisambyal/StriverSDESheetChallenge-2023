21dc04639527b4270b37310aa1f2c7c63be54ee1
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-06-20 15:16:27 +0530

    Create 5. Grid Unique Paths.cpp

---
+++ Day3: Arrays P-III/5. Grid Unique Paths.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> 
+int uniquePaths(int m, int n) {
+	
+	double ans = 1;
+	int N = m + n -2;
+	int r = m-1;
+
+	for(int i=1; i<=r; i++){
+		ans = ans * (N-r+i)/i;
+	}
+	return (int) ans;
+}
