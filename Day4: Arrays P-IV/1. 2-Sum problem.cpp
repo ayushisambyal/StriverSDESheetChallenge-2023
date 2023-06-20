d6791c4792e71a6a995836306692c3a1ac76e86f
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-06-20 18:18:30 +0530

    Update 1. 2-Sum problem.cpp

--- Day4: Arrays P-IV/1. 2-Sum problem.cpp
+++ Day4: Arrays P-IV/1. 2-Sum problem.cpp
@@ -1,1 +1,32 @@
+#include <bits/stdc++.h>
 
+vector<vector<int>> pairSum(vector<int> &arr, int s){
+   
+   int n = arr.size();
+   int left = 0, right = n-1;
+   vector<vector<int>> ans;
+   sort(arr.begin(), arr.end());
+
+   while (left < right) {
+      
+      int sum = arr[left] + arr[right];
+      if (sum == s) {
+      
+         int currLeft = left;
+         int currRight = right;
+         
+         // Find all possible pairs with the same sum
+         while (left < right && arr[left] == arr[currLeft]) {
+            while (left < right && arr[right] == arr[currRight]) {
+              ans.push_back({arr[left], arr[right]});
+              right--;
+            }
+            left++;
+            right = currRight;
+         }
+      }
+      else if (sum < s) left++;
+      else right--;
+   }
+   return ans;
+}
