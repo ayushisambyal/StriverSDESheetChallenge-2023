c6e4366a23b29ddc1ec14925c7b58e7a396710f8
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-06-20 22:13:14 +0530

    Update 3. Longest consecutive sequence.cpp

--- Day4: Arrays P-IV/3. Longest consecutive sequence.cpp
+++ Day4: Arrays P-IV/3. Longest consecutive sequence.cpp
@@ -1,1 +1,24 @@
+#include <bits/stdc++.h>
 
+int lengthOfLongestConsecutiveSequence(vector<int> &nums, int n) {
+    
+    if(n==0) return 0;
+    int longest = 1;
+    unordered_set<int> s;
+
+    for(auto it : nums){
+        s.insert(it);
+    }
+
+    for(auto it : s){
+        if(s.find(it - 1) == s.end()){
+            int cnt = 1, x = it;
+            while(s.find(x+1) != s.end()){
+                x++; cnt++;
+            }
+            longest = max(longest, cnt);
+        }
+    }
+
+    return longest;
+}
