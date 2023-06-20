4cb86c71e6d3a7150f927a1571b286ca837f237a
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-06-20 10:29:15 +0530

    Update 4. Majority element II.cpp

--- Day3: Arrays P-III/4. Majority element II.cpp
+++ Day3: Arrays P-III/4. Majority element II.cpp
@@ -1,1 +1,38 @@
+#include <bits/stdc++.h> 
+vector<int> majorityElementII(vector<int> &arr)
+{
+    int n=arr.size();
+    int ele1=0,cnt1=0, ele2=0,cnt2=0;
 
+    for(int i=0; i<n; i++){
+
+        if(cnt1==0 && ele2 != arr[i]){
+            cnt1=1;
+            ele1=arr[i];
+        }
+        else if(cnt2==0 && ele1 != arr[i]){
+            cnt2=1;
+            ele2=arr[i];
+        }
+
+        else if(ele1==arr[i]) cnt1++;
+        else if(ele2==arr[i]) cnt2++;
+        else{
+            cnt1--;
+            cnt2--;
+        }
+    }
+    
+    vector<int> ans;
+    cnt1=0;cnt2=0;
+
+    for(int i=0; i<n; i++){
+        if(arr[i] == ele1) cnt1++;
+        else if(arr[i] == ele2) cnt2++;
+    }
+
+    if(cnt1>n/3) ans.push_back(ele1);
+    if(cnt2>n/3) ans.push_back(ele2);
+    
+    return ans;
+}
