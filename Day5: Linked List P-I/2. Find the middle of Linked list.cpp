1ae421162a167776380895be6d60988402b7377d
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-06-22 00:39:22 +0530

    Update 2. Find the middle of Linked list.cpp

--- Day5: Linked List P-I/2. Find the middle of Linked list.cpp
+++ Day5: Linked List P-I/2. Find the middle of Linked list.cpp
@@ -1,1 +1,10 @@
+Node *findMiddle(Node *head) {
+    Node *slow = head;
+    Node *fast = head;
 
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
