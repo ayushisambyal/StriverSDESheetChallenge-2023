0db851ba44922b346080f0c61a1126f971f76be2
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-06-21 20:37:35 +0530

    Update 6. Longest substring without repeating char.cpp

--- Day4: Arrays P-IV/6. Longest substring without repeating char.cpp
+++ Day4: Arrays P-IV/6. Longest substring without repeating char.cpp
@@ -1,1 +1,15 @@
-
+#include <bits/stdc++.h> 
+int uniqueSubstrings(string s)
+{
+    vector<int> mp(256, -1);
+    int n = s.size(), r = 0, l = 0, len = 0;
+    
+    while(r < n){
+        if(mp[s[r]] != -1)
+            l = max(mp[s[r]]+1, l);
+        mp[s[r]] = r;
+        len = max(len, r-l+1);
+        r++;
+    }
+    return len;
+}
