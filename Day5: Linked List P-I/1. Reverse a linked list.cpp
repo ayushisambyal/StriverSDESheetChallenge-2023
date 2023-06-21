a188ac5b49203ec515d9a2ff7976773686fe9b84
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-06-21 23:46:12 +0530

    Update 1. Reverse a linked list.cpp

--- Day5: Linked List P-I/1. Reverse a linked list.cpp
+++ Day5: Linked List P-I/1. Reverse a linked list.cpp
@@ -1,1 +1,12 @@
+LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head) 
+{
+    LinkedListNode<int> *dummy = NULL;
 
+    while(head != NULL){
+        LinkedListNode<int> *next = head->next;
+        head->next = dummy;
+        dummy = head;
+        head = next;
+    }
+    return dummy;
+}
