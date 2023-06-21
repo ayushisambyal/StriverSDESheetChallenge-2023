660d8711c1a7fe19b90581c819899bb3535f5b2a
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-06-21 18:30:50 +0530

    Update 5. Count number of subarrays with given xor K.cpp

--- Day4: Arrays P-IV/5. Count number of subarrays with given xor K.cpp
+++ Day4: Arrays P-IV/5. Count number of subarrays with given xor K.cpp
@@ -1,1 +1,17 @@
+#include <bits/stdc++.h>
 
+int subarraysXor(vector<int> &arr, int k)
+{
+    int xr = 0;
+    map<int, int> mp;
+    mp[xr]++; // initialised with {0,1}
+
+    int len = 0;
+    for(int i=0; i<arr.size(); i++){
+        xr = xr ^ arr[i];
+        int x = xr ^ k;
+        len += mp[x];
+        mp[xr]++;
+    }
+    return len;
+}
