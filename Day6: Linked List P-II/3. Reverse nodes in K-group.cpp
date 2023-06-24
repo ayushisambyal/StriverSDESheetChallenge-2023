ee9579cd7e1603136f2a634c17becf394c0e5592
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-06-25 00:21:43 +0530

    Update 3. Reverse nodes in K-group.cpp

--- Day6: Linked List P-II/3. Reverse nodes in K-group.cpp
+++ Day6: Linked List P-II/3. Reverse nodes in K-group.cpp
@@ -1,1 +1,24 @@
-
+Node *getListAfterReverseOperation(Node *head, int n, int b[]){
+	
+	if(n==0 || head==NULL || head->next==NULL) return head;
+    
+	int cnt = b[0];
+    if(cnt == 0){
+        return getListAfterReverseOperation(head, n-1,b+1);
+    }
+    Node *temp=head, *prev=NULL;
+	
+    while(cnt>0 && temp!=NULL){
+        Node* n= temp->next;
+        temp->next=prev;
+        prev=temp;
+        temp=n;
+        cnt--;
+    }
+    if(cnt>0) return prev;
+    
+    Node* newHead= getListAfterReverseOperation(temp, n-1,b+1);
+    head->next=newHead;
+    
+    return prev;
+}
