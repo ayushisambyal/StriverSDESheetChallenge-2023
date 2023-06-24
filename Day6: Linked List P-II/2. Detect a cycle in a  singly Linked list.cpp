27f95a21aa425bc6ea6c4d5d29046a0eb0fd027a
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-06-24 23:44:33 +0530

    Update 2. Detect a cycle in a  singly Linked list.cpp

--- Day6: Linked List P-II/2. Detect a cycle in a  singly Linked list.cpp
+++ Day6: Linked List P-II/2. Detect a cycle in a  singly Linked list.cpp
@@ -1,1 +1,13 @@
+bool detectCycle(Node *head)
+{
+	if(head == NULL || head->next == NULL) return false;
+    Node *fast = head;
+    Node *slow = head;
 
+    while(fast->next && fast->next->next){
+        fast = fast->next->next;
+        slow = slow->next;
+        if(fast == slow) return true;
+    }
+    return false;
+}
