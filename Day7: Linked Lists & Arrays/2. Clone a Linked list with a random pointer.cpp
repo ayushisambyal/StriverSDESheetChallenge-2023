ef1995434180bebc001ea0360e00d62d181ba66f
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-06-26 16:59:37 +0530

    Update 2. Clone a Linked list with a random pointer.cpp

--- Day7: Linked Lists & Arrays/2. Clone a Linked list with a random pointer.cpp
+++ Day7: Linked Lists & Arrays/2. Clone a Linked list with a random pointer.cpp
@@ -1,1 +1,41 @@
+LinkedListNode<int> *cloneRandomList(LinkedListNode<int> *head)
+{
+    // 1- create deep copy nodes
+    LinkedListNode<int> *itr = head;
+    LinkedListNode<int> *front = head;
 
+    while(itr != NULL){
+        front = itr->next;
+        LinkedListNode<int> *copy = new LinkedListNode<int>(itr->data);
+        itr->next = copy;
+        copy->next = front;
+        itr = front;
+    }
+    
+    // 2- random pointers 
+    itr = head;
+    while(itr != NULL){
+        if(itr->random != NULL){
+            itr->next->random = itr->random->next;
+        }
+        else{
+            itr->next->random = NULL;
+        }
+        itr = itr->next->next;
+    }
+    
+    // 3- sggregate both the lls and return deep copy's head
+    itr = head; 
+    LinkedListNode<int> *dummy = new LinkedListNode<int>(0);
+    LinkedListNode<int> *copy = dummy;
+
+    while(itr != NULL){
+        front = itr->next->next;
+        copy->next = itr->next;
+        itr->next = front;
+        copy = copy->next;
+        itr = itr->next;
+    }
+    return dummy->next;
+}
+ 
