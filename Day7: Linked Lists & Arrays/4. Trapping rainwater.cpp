7ed19e1498178fdd017fc49b8aa0a89c442aade4
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-06-26 22:47:43 +0530

    Update 4. Trapping rainwater.cpp

--- Day7: Linked Lists & Arrays/4. Trapping rainwater.cpp
+++ Day7: Linked Lists & Arrays/4. Trapping rainwater.cpp
@@ -1,1 +1,20 @@
+#include <bits/stdc++.h> 
+long getTrappedWater(long *arr, int n){
+    
+    long l=0, r=n-1, res=0;
+    long lmax=INT_MIN, rmax=INT_MIN;
 
+    while( l <= r ){
+        if(arr[l] <= arr[r]){
+            if(arr[l]>=lmax) lmax=arr[l];
+            else res += lmax-arr[l];
+            l++;
+        }
+        else{
+            if(arr[r]>=rmax) rmax=arr[r];
+            else res += rmax-arr[r];
+            r--;
+        }
+    }
+    return res;
+}
