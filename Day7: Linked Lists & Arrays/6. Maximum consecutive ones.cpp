60de163ca804c587605b45d68e43e6139f3046bb
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-06-27 11:29:18 +0530

    Update 6. Maximum consecutive ones.cpp

--- Day7: Linked Lists & Arrays/6. Maximum consecutive ones.cpp
+++ Day7: Linked Lists & Arrays/6. Maximum consecutive ones.cpp
@@ -1,1 +1,24 @@
-
+int longestSubSeg(vector<int> &arr , int n, int k){
+    int i=0, j=0;
+    int kmx=k, ans=0;
+    while(i<n && j<n){
+        if(arr[j]==1){
+            j++;
+            ans=max(j-i, ans);
+        }else{
+            if(k){
+                k--;
+                ans=max(ans, j-i+1);
+                j++;
+            }else{
+                if(arr[i]==0){
+                    if(k<kmx){
+                        k++;
+                    }
+                }
+                i++;
+            }
+        }
+    }
+    return ans;
+}
