d76d6d3e5c7b8a83f1893ee87bfa6065962519aa
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-06-27 18:42:46 +0530

    Update 4. Fractional Knapsack.cpp

--- Day8: Greedy Algorithm/4. Fractional Knapsack.cpp
+++ Day8: Greedy Algorithm/4. Fractional Knapsack.cpp
@@ -1,1 +1,21 @@
+#include <bits/stdc++.h> 
 
+// comparing the val /wt ratio
+bool comp(pair<int, int> p1, pair<int, int> p2){
+    return p1.second/ (double)p1.first > p2.second/ (double) p2.first;
+}
+
+double maximumValue (vector<pair<int, int>>& items, int n, int w){
+    // ITEMS contains {weight, value} pairs.
+    sort(items.begin(), items.end(), comp);
+    double ans = 0.0;
+    for (auto it : items){
+        
+        if(w == 0) return ans;
+        else{
+            ans += min(w, it.first) * (it.second/ (double) it.first);
+            w -= min (it.first, w);
+        }
+    }
+    return ans;
+}
