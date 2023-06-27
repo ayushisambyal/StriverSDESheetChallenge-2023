cff6c4fb6380e495f2c33d7486f340dc19763885
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-06-27 20:46:21 +0530

    Update 1. Subset sums.cpp

--- Day9: Recursion/1. Subset sums.cpp
+++ Day9: Recursion/1. Subset sums.cpp
@@ -1,10 +1,11 @@
-#include <bits.c++/h>
+#include <bits/stdc++.h>
 
-fxn(int idx, int sum, vector<int> arr, int N, vector<int> ans){
-	if(idx == N)
+void fxn(int idx, int sum, vector<int> &arr, int N, vector<int> &ans){
+	if (idx == N) {
 		ans.push_back(sum);
 		return;
-	// pick
+	}
+        // pick
 	fxn(idx+1, sum+arr[idx], arr, N, ans);
 	// not pick
 	fxn(idx+1, sum, arr, N, ans);
@@ -14,5 +15,5 @@ vector<int> subsetSum(vector<int> &num){
 	vector<int> ans;
 	fxn(0, 0, num, num.size(), ans);
 	sort(ans.begin(), ans.end());
-	return sums;	
+	return ans;	
 }
