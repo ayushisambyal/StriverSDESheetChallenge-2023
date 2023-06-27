c9ed70f9e31eab4b8bb7d066898f32d9d4265b2d
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-06-27 23:17:35 +0530

    Update 2. Subset sum II.cpp

--- Day9: Recursion/2. Subset sum II.cpp
+++ Day9: Recursion/2. Subset sum II.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h> 
 
-void findsubs(int ind, vector<int> &nums, vector<int> ds, vector<vector<int>> ans){
+void findsubs(int ind, vector<int> &nums, vector<int> &ds, vector<vector<int>> &ans){
     ans.push_back(ds);
 
-    for(int i=0; i<nums.size(); i++){
+    for(int i=ind; i<nums.size(); i++){
         if(i != ind && nums[i]==nums[i-1]) continue;
         ds.push_back(nums[i]);
+        findsubs(i+1, nums, ds, ans);
+        ds.pop_back();
     }
-    findsubs(ind+1, nums, ds, ans);
-    ds.pop_back();
 }
 
 vector<vector<int>> uniqueSubsets(int n, vector<int> &arr)
