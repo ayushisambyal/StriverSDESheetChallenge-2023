98c893d926cc4d8f32e9eb6e0b093e074e94cec8
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-06-27 14:16:08 +0530

    Update 2. Minimum number of Platforms.cpp

--- Day8: Greedy Algorithm/2. Minimum number of Platforms.cpp
+++ Day8: Greedy Algorithm/2. Minimum number of Platforms.cpp
@@ -1,1 +1,20 @@
+int calculateMinPatforms(int at[], int dt[], int n) {
+    sort(at, at+n);
+    sort(dt, dt+n);
 
+    int plat=1, res=1;
+    int i=1, j=0;
+
+    while(i<n && j<n){
+        if(at[i] <= dt[j]){
+            plat++; i++;
+        }
+        else if(at[i] > dt[j]){
+            plat--; j++;
+        }
+        if(plat > res){
+            res = plat;
+        }
+    }
+    return res;
+}
