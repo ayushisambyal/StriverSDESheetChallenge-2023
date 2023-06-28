fca8285592146e996d2675fc8445a4a579ca231e
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-06-28 19:53:20 +0530

    Update 5. Palindrome partitioning.cpp

--- Day9: Recursion/5. Palindrome partitioning.cpp
+++ Day9: Recursion/5. Palindrome partitioning.cpp
@@ -1,1 +1,30 @@
+#include <bits/stdc++.h>
 
+bool isPalindrome(string &S, int s, int e){
+    while(s <= e){
+        if(S[s++] != S[e--]) return 0;
+    }
+    return 1;
+}
+
+void f(int ind, string &s, vector<string> &ds, vector<vector<string>> &res){
+    if(ind==s.size()){
+        res.push_back(ds);
+        return;
+    }
+    for(int i=ind; i<s.size(); i++){
+        if(isPalindrome(s, ind, i)){
+            ds.push_back(s.substr(ind, i-ind+1));
+            f(i+1, s, ds, res);
+            ds.pop_back();
+        }
+    }
+}
+
+vector<vector<string>> partition(string &s) 
+{
+    vector<vector<string>> ans;
+    vector<string> ds;
+    f(0, s, ds, ans);
+    return ans;
+}
