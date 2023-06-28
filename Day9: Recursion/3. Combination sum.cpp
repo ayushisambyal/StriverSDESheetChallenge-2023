84753eb2348cf10797bc3f7ae349eb28f8bb379b
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-06-28 17:30:12 +0530

    Update 3. Combination sum.cpp

--- Day9: Recursion/3. Combination sum.cpp
+++ Day9: Recursion/3. Combination sum.cpp
@@ -1,1 +1,22 @@
+void findC(int ind, int target, vector<vector<int>> &ans, vector<int> &arr, vector<int> &ds){
+    if(ind == arr.size()){
+        if(target==0){
+            ans.push_back(ds);
+        }
+        return;
+    }
+    if(arr[ind] <= target){
+        ds.push_back(arr[ind]);
+        findC(ind, target-arr[ind], ans, arr, ds);
+        ds.pop_back();
+    }
+    findC(ind+1, target, ans, arr, ds);
+}
 
+vector<vector<int>> combSum(vector<int> &arr, int B)
+{
+    vector<int> ds;
+    vector<vector<int>> ans;
+    findC(0, B, ans, arr, ds);
+    return ans;
+}
