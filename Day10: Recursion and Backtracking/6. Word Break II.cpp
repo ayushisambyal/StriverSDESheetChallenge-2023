cd189b54dfda8b310c13bc6ea72bb0d6a7e53015
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-07-05 20:57:14 +0530

    Create 6. Word Break II.cpp

---
+++ Day10: Recursion and Backtracking/6. Word Break II.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+void f(int idx, string &s, unordered_set<string> &st, vector<string> &ans, string cur){
+    if(idx==s.length()){
+        ans.push_back(cur);
+        return;
+    }
+    
+    for(int i=idx; i<s.length(); i++){
+        string temp= s.substr(idx,i-idx+1);
+        if(st.find(temp)!=st.end()){
+            f(i+1, s, st, ans, cur+temp+" ");
+        }
+    }
+    
+}
+
+vector<string> wordBreak(string &s, vector<string> &dictionary)
+{
+    unordered_set<string> st;
+    for(auto i : dictionary) st.insert(i);
+    vector<string> ans;
+    string cur="";
+    f(0,s,st,ans,cur);
+    return ans;
+}
