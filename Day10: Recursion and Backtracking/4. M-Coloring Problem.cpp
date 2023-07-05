a667acaea99d0d92755bc94dbb3c38b1803cf992
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-07-05 18:08:33 +0530

    Update 4. M-Coloring Problem.cpp

--- Day10: Recursion and Backtracking/4. M-Coloring Problem.cpp
+++ Day10: Recursion and Backtracking/4. M-Coloring Problem.cpp
@@ -1,1 +1,29 @@
+bool isSafe(int node, int color[], vector<vector<int>> &mat, int n, int col){
+    for(int i=0; i<n; i++){
+        if(i != node && mat[i][node] == 1 && color[i] == col){
+            return false;
+        }
+    }
+    return true;
+}
 
+bool f(int node, int color[], int m, int n, vector<vector<int>> &mat){
+    // base case
+    if(node == n) return true;
+    
+    for(int i=1; i<=m; i++){
+        if(isSafe(node, color, mat, n ,i)){
+            color[node] = i;
+            if(f(node+1, color, m, n, mat)) return true;
+            color[node] = 0;
+        }
+    }
+    return false;
+}
+
+string graphColoring(vector<vector<int>> &mat, int m) {
+    int n = mat.size();
+    int color[n] = {0};
+    if(f(0, color, m, n, mat)) return "YES";
+    return "NO";
+}
