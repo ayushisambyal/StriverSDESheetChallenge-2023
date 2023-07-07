7f1dee4075a3ce9d003b986ca02de0e09a038150
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-07-07 12:21:36 +0530

    Update 3. Search in Rotated sorted array.cpp

--- Day11: Binary Search/3. Search in Rotated sorted array.cpp
+++ Day11: Binary Search/3. Search in Rotated sorted array.cpp
@@ -1,1 +1,24 @@
+int search(int* arr, int n, int key) {
+    int l = 0, h = n-1;
+    
+    while(l <= h){
+        int m = (l + h) >> 1;
+        if(arr[m] == key) return m;
 
+        // if left is sorted
+        if(arr[l] <= arr[m]){
+            if(key >= arr[l] && key <= arr[m]){
+                h = m-1;
+            }
+            else l = m+1;
+        }
+        // if left is not sorted
+        else{
+            if(key >= arr[m] && key <= arr[h]){
+                l = m+1;
+            }
+            else h = m-1;
+        }
+    }
+    return -1;
+}
