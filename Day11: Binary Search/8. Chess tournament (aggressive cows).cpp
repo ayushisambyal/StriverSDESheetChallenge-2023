5e8c8b206b69facb93ace90c7498a84c2561128b
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-07-10 18:24:45 +0530

    Create 8. Chess tournament (aggressive cows).cpp

---
+++ Day11: Binary Search/8. Chess tournament (aggressive cows).cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h> 
+
+bool ispossible(vector<int> &arr, int minD, int c){
+	int cnt = 1, lastposition = arr[0];
+
+	for(int i=0; i<arr.size(); i++){
+		if(arr[i] -lastposition >= minD){
+			cnt++;
+			lastposition = arr[i];
+		}
+		if(cnt >= c) return 1;
+		else return 0;
+	}
+}
+
+int chessTournament(vector<int> positions , int n ,  int c){
+	sort(positions.begin(), positions.end());
+	int low = 0, high = positions[n-1] - positions[0];
+
+	while(low <= high){
+		int mid = (low + high) /2;
+		if(ispossible(positions, mid, c) == 1){
+			int ans = mid;
+			low = mid+1;
+		}
+		else high = mid-1;
+	}
+	return high;
+}
