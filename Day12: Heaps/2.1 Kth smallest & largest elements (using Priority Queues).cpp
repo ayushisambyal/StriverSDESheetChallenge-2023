404d23b253083068c9841c7118ebb0e17e535df1
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-07-19 18:42:22 +0530

    Update 2.1 Kth smallest & largest elements (using Priority Queues).cpp

--- Day12: Heaps/2.1 Kth smallest & largest elements (using Priority Queues).cpp
+++ Day12: Heaps/2.1 Kth smallest & largest elements (using Priority Queues).cpp
@@ -42,3 +42,27 @@ vector<int> kthSmallLarge(vector<int> &arr, int n, int k)
 	return res;
 
 }
+
+
+/*
+THIS IS ALSO VALID 
+
+#include <bits/stdc++.h> 
+
+vector<int> kMaxSumCombination(vector<int> &a, vector<int> &b, int n, int k){
+	priority_queue<int>pq;
+    vector<int>v;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            pq.push(a[i]+b[j]);
+        }
+    }
+    for(int i=0;i<k;i++){
+        int a=pq.top();
+        pq.pop();
+        v.push_back(a);
+    }
+    return v;
+} 
+ 
+ */
