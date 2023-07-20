0a1405828c14c658a09f2faabc42a2410c17cfd6
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-07-20 17:24:05 +0530

    Create 4. Largest rectangle in Histogram.cpp

---
+++ Day14: Stack and Queue P-II/4. Largest rectangle in Histogram.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+ int largestRectangle(vector < int > & heights) {
+  stack<int> st;
+  int maxA = 0;
+  int n = heights.size();
+
+  for(int i=0; i<=n; i++)
+  {
+    while(!st.empty() && (i==n || heights[st.top()] >= heights[i]))
+    {
+      int height = heights[st.top()];
+      st.pop();
+      int width;
+      if(st.empty()) width = i;
+      else width = i - st.top()-1;
+      maxA = max(maxA, width * height);
+    }
+    st.push(i);
+  }
+  return maxA;
+ }
