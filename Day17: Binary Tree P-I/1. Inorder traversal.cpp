b9fc336a8fb0fefddce600b2e248d79b28290aa8
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-07-23 17:30:16 +0530

    Update 1. Inorder traversal.cpp

--- Day17: Binary Tree P-I/1. Inorder traversal.cpp
+++ Day17: Binary Tree P-I/1. Inorder traversal.cpp
@@ -1,1 +1,27 @@
+#include <bits/stdc++.h> 
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
+vector<int> getInOrderTraversal(TreeNode *root)
+{
+    vector<int> ans;
+    if(root == NULL) return ans;
+    vector<int> l = getInOrderTraversal(root->left);
+    ans.insert(ans.end(), l.begin(), l.end());
 
+    ans.push_back(root->data);
+
+    vector<int> r = getInOrderTraversal(root->right);
+    ans.insert(ans.end(), r.begin(), r.end());
+
+    return ans;
+}
