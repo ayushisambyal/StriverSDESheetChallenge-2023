6f3d211a37b949dab2e39a20669322af7f7ac1a5
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-07-23 18:41:15 +0530

    Update and rename 04. to 04. Morris Traversal.cpp

---
+++ Day17: Binary Tree P-I/04. Morris Traversal.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h> 
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
+vector<int> getInOrderTraversal(TreeNode *root)
+{
+    vector<int> ans;
+    TreeNode * cur = root;
+    while(cur != NULL){
+        // base case 1: if left doesn't exist
+        if(cur->left == NULL){
+            ans.push_back(cur->data);
+            cur = cur-> right;
+        }
+        // when left exists - threded binary tree
+        else{
+            TreeNode *temp = cur->left;
+            // finding the rightmost node
+            while(temp->right && temp->right != cur){
+                temp = temp->right;
+            }
+            // if we reached rightmost - create the thread
+            if(temp->right == NULL){
+                temp->right = cur;
+                cur = cur->left; // move left
+            }
+            // 2nd traversal - time to break threads
+            else{
+                temp->right = NULL;
+                ans.push_back(cur->data);
+                cur = cur->right;
+            }
+        }
+    }
+    return ans;
+}
