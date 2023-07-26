3913cb86e66791da4bd98e3b87eb9ce38348e848
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-07-26 19:04:07 +0530

    Update 4. Is Height Balanced BT.cpp

--- Day 18: Binary Tree P-II/4. Is Height Balanced BT.cpp
+++ Day 18: Binary Tree P-II/4. Is Height Balanced BT.cpp
@@ -1,1 +1,15 @@
+int dfsh(BinaryTreeNode<int> *root){
+    if(root == NULL) return 0;
+    
+    int lh = dfsh(root->left);
+    if(lh == -1) return -1;
+    int rh = dfsh(root->right);
+    if(rh == -1) return -1;
 
+    if(abs(lh - rh) > 1) return -1;
+    return max(lh, rh) + 1;
+}
+
+bool isBalancedBT(BinaryTreeNode<int>* root) {
+    return dfsh(root) != -1;
+}
