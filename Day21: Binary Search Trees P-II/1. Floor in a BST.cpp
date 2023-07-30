b20050c358f337f296d6a39f1233472ed4f48260
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-07-31 03:06:13 +0530

    Update 1. Floor in a BST.cpp

--- Day21: Binary Search Trees P-II/1. Floor in a BST.cpp
+++ Day21: Binary Search Trees P-II/1. Floor in a BST.cpp
@@ -1,1 +1,37 @@
+#include <bits/stdc++.h> 
+/************************************************************
 
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T val;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+int floorInBST(TreeNode<int> * root, int x)
+{
+    int floor = -1;
+    while(root){
+        if(root->val == x){
+            floor = root->val;
+            return floor;
+        }
+        if(x > root->val){
+            floor = root->val;
+            root = root->right;
+        }
+        else root = root->left;
+    }
+    return floor;
+}
