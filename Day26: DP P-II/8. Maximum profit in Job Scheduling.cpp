85f10761b186927f53f39ad803a65f671687158f
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-07-31 17:07:33 +0530

    Update 8. Maximum profit in Job Scheduling.cpp

--- Day26: DP P-II/8. Maximum profit in Job Scheduling.cpp
+++ Day26: DP P-II/8. Maximum profit in Job Scheduling.cpp
@@ -1,1 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+vector<int> jobScheduling(vector<vector<int>>& jobs) {
+    int n = jobs.size();
+
+    sort(jobs.begin(), jobs.end(), [](const vector<int>& a, const vector<int>& b) {
+        return a[2] > b[2];
+    });
+
+    vector<bool> slot(n, false);
+    vector<int> result(2, 0); 
+
+    for (int i = 0; i < n; i++) {
+        for (int j = min(n, jobs[i][1]) - 1; j >= 0; j--) {
+            if (!slot[j]) {
+                slot[j] = true;
+                result[0]++;
+                result[1] += jobs[i][2];
+                break;
+            }
+        }
+    }
+
+    return result;
+}
