3ec9d8a10333b5a267190f9695870f8bc32225e3
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-07-31 19:16:49 +0530

    Update 5. Partial BST - Validate a BST.cpp

--- Day20: Binary Search Tree P-I/5. Partial BST - Validate a BST.cpp
+++ Day20: Binary Search Tree P-I/5. Partial BST - Validate a BST.cpp
@@ -1,29 +1,30 @@
-#include <bits/stdc++.h> 
 /*************************************************************
  
     Following is the Binary Tree node structure
 
+    template<typename T>
     class BinaryTreeNode 
     {
-    public : 
+    public: 
         T data;
         BinaryTreeNode<T> *left;
         BinaryTreeNode<T> *right;
 
         BinaryTreeNode(T data) {
-            this -> data = data;
+            this->data = data;
             left = NULL;
             right = NULL;
         }
     };
 
 *************************************************************/
-bool f(BinaryTreeNode<int> *root, long int lb, long int ub){
-    if(root == NULL) return true;
-    if(root->data >= ub || root->data <= lb) return false;
+
+bool f(BinaryTreeNode<int>* root, long long lb, long long ub) {
+    if (root == NULL) return true;
+    if (root->data > ub || root->data <= lb) return false;
     return f(root->left, lb, root->data) && f(root->right, root->data, ub);
 }
 
-bool validateBST(BinaryTreeNode<int> *root) {
-    return f(root, INT_MIN, INT_MAX);
+bool validateBST(BinaryTreeNode<int>* root) {
+    return f(root, LLONG_MIN, LLONG_MAX);
 }
