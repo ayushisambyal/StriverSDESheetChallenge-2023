24ffb5ed168f037ccd34167b5b5641be8347c812
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-07-31 09:34:40 +0530

    Update 1. Convert a given BT to Doubly Linked List.cpp

--- Day22: Binary Tree [Misc]/1. Convert a given BT to Doubly Linked List.cpp
+++ Day22: Binary Tree [Misc]/1. Convert a given BT to Doubly Linked List.cpp
@@ -1,1 +1,49 @@
+#include <bits/stdc++.h> 
+/*************************************************************
+ 
+    Following is the Binary Tree node structure
 
+    class BinaryTreeNode 
+    {
+    public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
+
+BinaryTreeNode<int>* BTtoDLL(BinaryTreeNode<int>* root) {
+    BinaryTreeNode<int>* prev = NULL;
+    BinaryTreeNode<int>* head = NULL;
+
+    function<void(BinaryTreeNode<int>*)> inorderTraversal = [&](BinaryTreeNode<int>* root) {
+        if (!root) return;
+
+        inorderTraversal(root->left);
+
+        if (!prev) {
+            // This is the first node of DLL, so update the head
+            head = root;
+        } else {
+            // Rewire the pointers to create the DLL
+            prev->right = root;
+            root->left = prev;
+        }
+
+        // Update prev to the current node for the next iteration
+        prev = root;
+
+        inorderTraversal(root->right);
+    };
+
+    inorderTraversal(root);
+
+    return head;
+}
