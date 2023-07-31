49eb5bb0701f9791e79d70aee8cfd8f7c09fb1af
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-07-31 16:39:06 +0530

    Update 3. Longest Common Subsequence.cpp

--- Day25: DP P-I/3. Longest Common Subsequence.cpp
+++ Day25: DP P-I/3. Longest Common Subsequence.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int lcs(string s, string t) {
+    int m = s.length();
+    int n = t.length();
+
+    // Initialize a 2D DP array to store the LCS lengths
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (s[i - 1] == t[j - 1]) {
+                // If the characters match, increment LCS length by 1
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                // If characters don't match, take the maximum LCS length so far
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    // The last cell in the DP array contains the length of the LCS
+    return dp[m][n];
+}
