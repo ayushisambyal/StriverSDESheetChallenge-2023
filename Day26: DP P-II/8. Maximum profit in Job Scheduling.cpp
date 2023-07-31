dd73ab2ffc73e034bf5fc20930ee3c42ca363040
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-07-31 19:08:49 +0530

    Update 8. Maximum profit in Job Scheduling.cpp

--- Day26: DP P-II/8. Maximum profit in Job Scheduling.cpp
+++ Day26: DP P-II/8. Maximum profit in Job Scheduling.cpp
@@ -1,26 +1,60 @@
+/*
+    Time Complexity : O(N *log max(N, maxDeadline))
+    Space Complexity : O(maxDeadline)
+
+    Where 'N' is size of the array "jobs" and
+    'maxDeadline' is the maximum among all the deadlines.
+*/
+
 #include <bits/stdc++.h>
-using namespace std;
 
-vector<int> jobScheduling(vector<vector<int>>& jobs) {
-    int n = jobs.size();
+// Custom Comparator function to sort the jobs in the decreasing order of their profit.
+bool compare(vector<int> &job1, vector<int> &job2)
+{
+    return job1[2] > job2[2];
+}
 
-    sort(jobs.begin(), jobs.end(), [](const vector<int>& a, const vector<int>& b) {
-        return a[2] > b[2];
-    });
+vector<int> jobScheduling(vector<vector<int>> &jobs)
+{
 
-    vector<bool> slot(n, false);
-    vector<int> result(2, 0); 
+    sort(jobs.begin(), jobs.end(), compare);
 
-    for (int i = 0; i < n; i++) {
-        for (int j = min(n, jobs[i][1]) - 1; j >= 0; j--) {
-            if (!slot[j]) {
-                slot[j] = true;
-                result[0]++;
-                result[1] += jobs[i][2];
-                break;
-            }
-        }
+    int maxProfit = 0;
+    int numberOfJobs = 0;
+    int maxDeadline = 0;
+
+    // Find the maximum deadline among all the jobs.
+    for (int i = 0; i < jobs.size(); i++)
+    {
+        maxDeadline = max(maxDeadline, jobs[i][1]);
     }
 
-    return result;
+    // Create a set "slots".
+    set<int, greater<int>> slots;
+
+    // Insert all the elements from maxDeadline to 1 into the set.
+    for (int i = maxDeadline; i > 0; i--)
+    {
+        slots.insert(i);
+    }
+    vector<int> ans;
+
+    for (int i = 0; i < jobs.size(); i++)
+    {
+
+        // If the set is empty or the deadline is less than the last element of the set, then ignore this job.
+        if (slots.size() == 0 || jobs[i][1] < *slots.rbegin())
+        {
+            continue;
+        }
+
+        int availableSlot = *slots.lower_bound(jobs[i][1]);
+        maxProfit = maxProfit + jobs[i][2];
+        numberOfJobs += 1;
+        slots.erase(availableSlot);
+    }
+
+    ans.push_back(numberOfJobs);
+    ans.push_back(maxProfit);
+    return ans;
 }
