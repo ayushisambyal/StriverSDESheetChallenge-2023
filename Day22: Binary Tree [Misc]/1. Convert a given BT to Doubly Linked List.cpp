ec671a464f3d811f56ddb788edd6068606ccd524
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-08-05 21:43:21 +0530

    Update 1. Convert a given BT to Doubly Linked List.cpp

--- Day22: Binary Tree [Misc]/1. Convert a given BT to Doubly Linked List.cpp
+++ Day22: Binary Tree [Misc]/1. Convert a given BT to Doubly Linked List.cpp
@@ -1,27 +1,52 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    void flatten(TreeNode* root) {
-        TreeNode* cur = root;
-        while(cur != NULL){
-            if(cur->left != NULL){
-                TreeNode* prev = cur->left;
-                while(prev->right) prev = prev->right;
-                prev->right = cur->right;
-                cur->right = cur->left;   
-                cur->left = NULL;             
-            }
-            cur = cur->right;
+#include <bits/stdc++.h> 
+/***************************************************************************
+
+	Class for graph node is as follows:
+
+	class graphNode
+	{
+		public:
+    		int data;
+    	vector<graphNode *> neighbours;
+    	graphNode()
+    	{
+        	data = 0;
+        	neighbours = vector<graphNode *>();
+    	}
+
+    	graphNode(int val)
+    	{
+        	data = val;
+        	neighbours = vector<graphNode *>();
+    	}
+
+    	graphNode(int val, vector<graphNode *> neighbours)
+    	{
+        	data = val;
+        	this->neighbours = neighbours;
+    	}
+	};
+
+******************************************************************************/
+void dfs(graphNode *node, graphNode *copy, unordered_map<int, graphNode*> &vis){
+    vis[copy->data] = copy;
+    for(auto it : node->neighbours){
+        if(vis.find(it->data) == vis.end()){ // Check if the node is not visited
+            graphNode *newnode = new graphNode(it->data);
+            copy->neighbours.push_back(newnode);
+            dfs(it, newnode, vis);
+        }
+        else{ // The node is already visited
+            copy->neighbours.push_back(vis[it->data]);
         }
     }
-};
+}
+
+graphNode *cloneGraph(graphNode *node)
+{
+    if(node == NULL) return NULL;
+    unordered_map<int, graphNode*> vis; // Use unordered_map for O(1) lookup
+    graphNode *copy = new graphNode(node->data);
+    dfs (node, copy, vis);
+    return copy; 
+}
