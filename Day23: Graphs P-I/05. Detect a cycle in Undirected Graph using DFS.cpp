a7a6154a06d62f20d8b2c241dcc3cd86a6e040f5
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-08-25 22:09:06 +0530

    Update 05. Detect a cycle in Undirected Graph using DFS.cpp

--- Day23: Graphs P-I/05. Detect a cycle in Undirected Graph using DFS.cpp
+++ Day23: Graphs P-I/05. Detect a cycle in Undirected Graph using DFS.cpp
@@ -1,26 +1,15 @@
 #include <bits/stdc++.h>
 
-bool isCycle(vector<int> &vis, vector<vector<int>> &adj, int idx, vector<vector<int>>&edges){
-    queue<pair<int,int>> q;
-    q.push({idx, -1});
-    vis[idx]++;
-    while(!q.empty()){
-        int node= q.front().first;
-        int pre= q.front().second;
-        q.pop();
-        for(auto i : adj[node]){
-            if(!vis[i]){
-                vis[i]++;
-                q.push({i, node});
-            }
-            else if(pre!=i){
-                return true;
-            }
+bool dfs(int node, int parent, vector<int> &vis, vector<vector<int>> &adj){
+    vis[node] = 1;
+    for(auto it : adj[node]){
+        if(vis[it] == 0){
+            if(dfs(it, node, vis, adj)) return true;
+            else if(it != parent) return true;
         }
     }
     return false;
 }
-
 
 string cycleDetection (vector<vector<int>>& edges, int n, int m)
 {
@@ -32,7 +21,7 @@ string cycleDetection (vector<vector<int>>& edges, int n, int m)
     }
     for(int i=1; i<=n; i++){
         if(!vis[i]){
-            if(isCycle(vis, adj, i, edges)) return "Yes";
+            if(dfs(i, -1, vis, adj)) return "Yes";
         }
     }
     return "No";
