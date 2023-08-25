52c98f1ea53c1566d75585a2e8786d01bb375b5a
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-08-25 11:56:35 +0530

    Update 03. BFS.cpp

--- Day23: Graphs P-I/03. BFS.cpp
+++ Day23: Graphs P-I/03. BFS.cpp
@@ -1,28 +1,21 @@
-void dfs(vector<vector<int>> &adj, vector<int> &ans, vector<int> &vis, int i){
-    vis[i]=1;
-    ans.push_back(i);
-    for(auto j : adj[i]){
-        if(vis[j]==0){
-            dfs(adj, ans, vis, j);
-        }
-    }
-}
+vector<int> bfsTraversal(int n, vector<vector<int>> &adj){
+    int vis[n] = {0};
+    vis[0] = 1;
+    queue<int> q;
+    q.push(0);
+    vector<int> bfs;
+    
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
+        bfs.push_back(node);
 
-vector<vector<int>> depthFirstSearch(int V, int E, vector<vector<int>> &edges)
-{
-    vector<vector<int>> adj(V);
-    for(int i=0; i<E; i++){
-        adj[edges[i][0]].push_back(edges[i][1]);
-        adj[edges[i][1]].push_back(edges[i][0]);
-    }
-    vector<int> visited(V,0);
-    vector<vector<int>> ans;
-    for(int i=0; i<V; i++){
-        vector<int> temp;
-        if(visited[i]==0){
-            dfs(adj, temp, visited, i);
+        for(auto it : adj[node]){
+            if(!vis[it]){
+                vis[it] = 1;
+                q.push(it);
+            }
         }
-        if(temp.size()>0) ans.push_back(temp);
     }
-    return ans;
+    return bfs;
 }
