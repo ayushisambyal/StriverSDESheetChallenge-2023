656d2396056a3899045e13b35af9784b2887a226
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-09-06 20:04:45 +0530

    Update 07. Detect a cycle in Directed Graph using BFS.cpp

--- Day23: Graphs P-I/07. Detect a cycle in Directed Graph using BFS.cpp
+++ Day23: Graphs P-I/07. Detect a cycle in Directed Graph using BFS.cpp
@@ -23,3 +23,48 @@ int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
     if(cnt==n) return false;
     return true;
 }
+
+
+// Method - 2 (partially correct)
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isCycle(int node, vector<int> adj[], int vis[], int dfsvis[]){
+  vis[node] = 1; dfsvis[node] = 1;
+
+  for(auto it : adj[node]){
+    if(!vis[it]){
+      if(isCycle(it, adj, vis, dfsvis)) return true;
+    }
+    else if(dfsvis[it]) return true;
+  }
+  dfsvis[node] = 0;
+  return false;
+}
+
+int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
+  // creating adjacency list from edges
+  vector<int> adj[n+1];
+  // for(int i=0; i<n; i++){
+  //   adj[edges[i][0]].push_back(edges[i][1]);
+  //   adj[edges[0][i]].push_back(edges[i][0]);
+  // }
+  for(int i = 0; i < edges.size(); i++) {
+    adj[edges[i].first].push_back(edges[i].second);
+  }
+
+  // create two visited arrays
+  int vis[n], dfsvis[n];
+  // set them both to zero
+  memset(vis, 0, sizeof vis);
+  memset(dfsvis, 0, sizeof dfsvis);
+
+  for(int i=1; i<=n; i++){
+    if(!vis[i]){
+      if(isCycle(i, adj, vis, dfsvis)){
+        return true;
+      }
+    }
+  }
+  return false;
+}
