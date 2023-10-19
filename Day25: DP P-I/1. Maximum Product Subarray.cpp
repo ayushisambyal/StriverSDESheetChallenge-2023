b93e46e9b1416d29d0f21d1530731d3c2069f3d2
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-10-19 15:52:17 +0530

    Update 1. Maximum Product Subarray.cpp

--- Day25: DP P-I/1. Maximum Product Subarray.cpp
+++ Day25: DP P-I/1. Maximum Product Subarray.cpp
@@ -1,6 +1,25 @@
 int maximumProduct(vector<int> &arr, int n)
 {
     int ans=INT_MIN;
+    int pre = 1, suff = 1;
+
+    for(int i=0; i<n; i++){
+        if(pre == 0) pre = 1;
+        if(suff == 0) suff = 1;
+
+        pre = pre * arr[i];
+        suff = suff * arr[n-i-1];
+        ans = max(ans, max(pre, suff));
+    }
+    return ans;
+}
+
+
+// METHOD 2 - variation of Kadane's algo
+
+int maximumProduct(vector<int> &arr, int n)
+{
+    int ans=INT_MIN;
     for(int i=0; i<n; i++){
         ans=max(ans, arr[i]);
     }
