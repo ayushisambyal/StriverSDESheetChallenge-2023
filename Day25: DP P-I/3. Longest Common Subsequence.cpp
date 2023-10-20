6c526c5f150f363d5be1feeabaaee05f2e251b92
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-10-20 08:19:43 +0530

    Update 3. Longest Common Subsequence.cpp

--- Day25: DP P-I/3. Longest Common Subsequence.cpp
+++ Day25: DP P-I/3. Longest Common Subsequence.cpp
@@ -1,3 +1,29 @@
+// After space optimization
+#include <bits/stdc++.h>
+using namespace std;
+
+int lcs(string s, string t) {
+    int n = s.size();
+    int m = t.size();
+
+    vector<int> prev(m+1, 0), cur(m+1, 0);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (s[i - 1] == t[j-1]) {
+                cur[j] = 1 + prev[j-1];
+            } else {
+                cur[j] = max(prev[j], cur[j-1]);
+            }
+        }
+        swap(prev, cur);
+    }
+    return prev[m];
+}
+
+
+
+// Using Tabulation - Bottom up
 #include <bits/stdc++.h>
 using namespace std;
 
