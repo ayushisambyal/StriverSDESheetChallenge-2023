68926f889d3024db04e9e9e0504cca2c7b26859b
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-10-23 21:44:17 +0530

    Update 4. 0-1 Knapsack.cpp

--- Day25: DP P-I/4. 0-1 Knapsack.cpp
+++ Day25: DP P-I/4. 0-1 Knapsack.cpp
@@ -1,3 +1,24 @@
+// Once Row Optimization
+#include<bits/stdc++.h>
+int maxProfit(vector<int> &val, vector<int> &wt, int n, int maxwt)
+{  
+    vector<int> prev(maxwt+1,0);
+    for(int w = wt[0]; w<=maxwt; w++) prev[w] = val[0];
+
+    for(int i=1; i<n; i++){
+        for(int w=maxwt; w>=0; w--){
+            int notTake = 0 + prev[w];
+			int take = INT_MIN;
+            if(wt[i] <= w){
+                take = val[i] + prev[w-wt[i]];
+            }
+            prev[w] = max(take, notTake);
+		}
+    }
+    return prev[maxwt];
+}
+
+// Two Row Optimized
 int maxProfit(vector<int> &values, vector<int> &weights, int n, int w)
 {    
     vector<int> prev(w+1,0), cur(w+1,0);
