ed71c4509ed74bd5a29edea8aa90408ea77bc529
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-10-24 17:34:59 +0530

    Update 7. Matrix Chain Multiplication.cpp

--- Day25: DP P-I/7. Matrix Chain Multiplication.cpp
+++ Day25: DP P-I/7. Matrix Chain Multiplication.cpp
@@ -1,3 +1,23 @@
+// Tabulation 
+#include <bits/stdc++.h> 
+int matrixMultiplication(vector<int> &arr, int N)
+{
+    int dp[N][N];
+    for(int i=1; i<N; i++) dp[i][i] = 0;
+    for(int i=N-1; i>=1; i--){
+        for(int j=i+1; j<N; j++){
+            int mini = 1e9;
+            for(int k=i; k<j; k++){
+                int steps = arr[i-1] * arr[k] * arr[j] + dp[i][k] + dp[k+1][j];
+                if(steps < mini) mini = steps;
+            }
+            dp[i][j] = mini;
+        }
+    }
+    return dp[1][N-1];
+}
+
+// Memoization
 #include <bits/stdc++.h>
 
 int f(vector<int> &arr, int i, int j, vector<vector<int>> &dp){
