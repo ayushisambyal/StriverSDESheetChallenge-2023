78861e954f6f9070d2c2467b589d658a524c8193
Ayushi Sambyal <96545024+ayushisambyal@users.noreply.github.com>
2023-10-28 10:59:14 +0530

    Update 1.2 Implement Trie.cpp

--- Day27: Trie/1.2 Implement Trie.cpp
+++ Day27: Trie/1.2 Implement Trie.cpp
@@ -78,3 +78,21 @@ class Trie{
         node->reduceEW();
     }
 };
+
+
+
+// without using Trie
+
+#include <bits/stdc++.h>
+string longestCommonPrefix(vector<string> &arr, int n)
+{
+    string ans="";
+    int sz=arr.size();
+    sort(arr.begin(), arr.end());
+    for(int i=0; i<arr[0].size(); i++){
+        if(arr[0][i]==arr[sz-1][i]){
+            ans+=arr[0][i];
+        }else break;
+    }
+    return ans;
+}
